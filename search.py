6a397d024d46c94148431f36e16b0540484e4c50
MyName <e175714@ie.u-ryukyu.ac.jp>
2019-08-27 19:36:37 +0900

    Improve Source code

--- search.py
+++ search.py
@@ -2,7 +2,7 @@
 import sys
 
 search_word = sys.stdin.readline()#探す値の入力値の設定
-conv_search_word = search_word.strip()
+conv_search_word = search_word.strip()#値を使いやすいように変換する
 
 filenames=['file1.txt','file2.txt','file3.txt']#ページのまとまり
 pagenum = len(filenames)#ページ数
@@ -13,14 +13,14 @@ for i in range(pagenum):
     word = lines[0].split()#文字列を1つづつに変換
     wordnum = len(word)
     for x in range(wordnum):
-        conv_word = word[x].strip()
-        if conv_search_word == conv_word:
-            answer.append(filenames[i])
+        conv_word = word[x].strip()#5行目と同じように綺麗な形にする
+        if conv_search_word == conv_word:#存在するかを比較して確かめる
+            answer.append(filenames[i])#あった場合に、ページ情報を追加する
             break
 
 if answer == []:
-    print("not exist.")
+    print("not exist.")#なかった場合
 else:
-    print(answer)
+    print(answer)#あった場合
 page.close()
 
